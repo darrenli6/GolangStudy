68fc2d0d9ec8af0027550f9036af98fadae739bd
darren <darren94me@gmail.com>
2022-05-23 10:41:24 +0800

    在线用户

--- 14-golang-IM-system/README.md
+++ 14-golang-IM-system/README.md
@@ -1,4 +1,45 @@
 
+# tag操作
+
+
+- 1. 在相应的本地仓库下打开git终端输入
+```
+git tag v1.0.0
+```
+
+- 2. 查看是否成功
+```
+git tag
+```
+
+- 3. 提交到远程
+```
+git push origin v1.0.0
+```
+
+
+- 1.删除本地tag
+```
+git tag -d tagName
+```
+- 2.删除远端tag
+```
+git push origin :refs/tags/tagName
+```
+
+- 3.创建本地tag
+```
+git tag tagName
+```
+
+- 4.提交到远端
+```
+git push origin --tags //提交全部tag
+或者
+git push origin tagName //提交单个tag
+```
+ 
+
 # 即实系统
 
 ## 服务端
--- 14-golang-IM-system/server.go
+++ 14-golang-IM-system/server.go
@@ -3,24 +3,69 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
+
+// 监听message广播消息channel的goroutine 一旦有消息就发送给全部在线的user
+func (this *Server) ListenMessage() {
+	for {
+
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个server接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+func (this *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handle(conn net.Conn) {
 	fmt.Println("连接建立成功")
+
+	user := NewUser(conn)
+
+	// 用户线上的了 将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	// 广播当前用户上线消息
+
+	this.Broadcast(user, "已经上线")
+
+	// 当前handler 阻塞
+
+	select {}
+
 }
 
 // 启动服务器的接口
@@ -35,6 +80,9 @@ func (this *Server) Start() {
 
 	// close
 	defer listener.Close()
+
+	// 启动监听
+	go this.ListenMessage()
 
 	for {
 		// accept
