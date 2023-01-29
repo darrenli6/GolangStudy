37ef2583f83f0c098b0d7cef906a68747090eedc
darren <darren94me@gmail.com>
2023-01-29 17:09:26 +0800

    m

---
+++ GoBasic/15-Context/test2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", SayHello)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
+
+func SayHello1(writer http.ResponseWriter, request *http.Request) {
+	//每个请求对应的Handler，常会启动额外的的goroutine进行数据查询或PRC调用
+	fmt.Println(&request)
+
+	// 没有对声明周期进行管理 context进行了管理
+	go func() {
+		for range time.Tick(time.Second) {
+			fmt.Println("current request is in progress ")
+		}
+	}()
+	time.Sleep(2 * time.Second)
+
+	writer.Write([]byte("hi new request come"))
+
+}
