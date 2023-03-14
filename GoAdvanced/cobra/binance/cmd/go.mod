4b30f417724c153e2e1c4ebc28e61df8fa4c7641
darren <darren94me@gmail.com>
2023-03-14 10:54:15 +0800

    m1

---
+++ GoAdvanced/cobra/binance/cmd/go.mod
@@ -0,0 +1,10 @@
+module github.com/darrenli6/GolangStudy/GoAdvanced/cobra/binance/cmd
+
+go 1.19
+
+require github.com/spf13/cobra v1.6.1
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- GoAdvanced/cobra/binance/cmd/main.go
+++ GoAdvanced/cobra/binance/cmd/main.go
@@ -1,1 +1,63 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// 收益曲线、
+//最大回撤、年化收益率、夏普比
+
+// 根命令
+var rootCmd = &cobra.Command{
+	Use:   "fbinance",
+	Short: "一个命令行应用程序",
+	Long:  "这是一个收益计算工具",
+}
+
+// 子命令
+var (
+	serverCmd = &cobra.Command{
+		Use:   "makeline",
+		Short: "生成收益率曲线",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: 在此处添加启动服务器的代码
+		},
+	}
+
+	dbCmd = &cobra.Command{
+		Use:   "maxdrawdown",
+		Short: "最大回撤",
+		Long:  "收益最大回撤",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: 在此处添加启动服务器的代码
+		},
+	}
+
+	// dbCmd 的子命令
+	dbMigrateCmd = &cobra.Command{
+		Use:   "年化收益率",
+		Short: "执行数据库迁移",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: 在此处添加数据库迁移的代码
+		},
+	}
+)
+
+func init() {
+	// 添加子命令
+	rootCmd.AddCommand(serverCmd)
+	rootCmd.AddCommand(dbCmd)
+
+	// 添加 dbCmd 的子命令
+	dbCmd.AddCommand(dbMigrateCmd)
+}
+
+func main() {
+	// 解析命令行参数并执行命令
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
